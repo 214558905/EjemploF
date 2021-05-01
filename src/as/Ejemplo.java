3d8e276053c7af51d5d4c254e36c6e067d37e78e
ales <alexnaula57@gmail.com>
2021-05-01 11:06:09 -0500

    Create new class

---
+++ src/as/Ejemplo.java
@@ -0,0 +1,16 @@
+package as;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x,y;
+		x=2;
+		y=3;
+		
+		System.out.println(x+y);
+
+	}
+
+}
